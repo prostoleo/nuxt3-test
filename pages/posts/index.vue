f18fb3256e672907a577e88328d4153f6598a9d9
prostoleo <73364400+prostoleo@users.noreply.github.com>
2022-05-09 20:44:59 +0300

    second commit

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,9 +1,9 @@
 <template>
-	<div class="container mx-auto">
+	<div class="container mx-auto px-3">
 		<h1>это посты</h1>
 		<!-- <pre v-if="!pending" >
       </pre> -->
-		<ul class="grid grid-cols-3 gap-3" v-if="!pending">
+		<ul class="grid gap-5 sm:(grid-cols-2) md:(grid-cols-3)" v-if="!pending">
 			<PostCard v-for="post in data" :post="post" :key="post.id"></PostCard>
 		</ul>
 	</div>
